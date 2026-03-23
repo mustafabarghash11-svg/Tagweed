7a9e2a50fe39dad75beb2b73c279245f563769f3
mustafabarghash11-svg <mustafabarghash11@gmail.com>
2026-03-23 19:15:51 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// تسجيل Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' })
-      .catch((err) => console.warn('SW registration failed:', err));
-  });
-}
+
 
 createRoot(document.getElementById("root")!).render(<App />);
